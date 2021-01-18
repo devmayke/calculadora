3b8ef18c3571d0f0cd263ca6b4ada969990f71b8
Mayke Ken <devmayke@gmail.com>
2021-01-18 17:14:59 -0300

    refatorando

--- javascript.js
+++ javascript.js
@@ -1,24 +1,31 @@
 for(let i = 0; i < 18; i++){
-    document.querySelectorAll('button')[i].addEventListener("click", function(){        
+    document.querySelectorAll('button')[i].addEventListener("click", function(){         
         if(!isNaN(this.innerText)){        
-        let tela = document.querySelector('.resultado');
-        tela.innerText += this.innerText;
-        }else{  
+            let tela = document.querySelector('.resultado');
+            tela.innerText += this.innerText;
+        }else{ 
+            let cont = true; 
             let tela = document.querySelector('.resultado');         
-            operador = this.innerText; 
-            if(operador === 'Enter' && tela.innerText === ''){
-            tela.innerText = '';
-            }else if(operador === 'Enter'){
-                tela.innerText = telaFinalResultado();
-                console.log(telaFinalResultado());
-            }else if(operador === 'C'){
-                tela.innerText = '';
-            }else if(operador === ','){
-                tela.innerText += '.';
-            }else{           
-                tela.innerText += operador; 
-        }       
-    }
+            operador = this.innerText;             
+            switch (operador){               
+                case 'Enter':
+                    if(operador === 'Enter' && tela.innerText === ''){
+                        tela.innerText = '';
+                    }else{
+                        tela.innerText = telaFinalResultado();
+                        console.log(telaFinalResultado());
+                    }
+                    break;
+                case 'C':
+                    tela.innerText = '';                
+                    break;
+                case ',':
+                    tela.innerText += '.';                   
+                    break;
+                default:
+                    tela.innerText += operador;            
+            }
+        }
     function telaFinalResultado(){
         let telaFinal = document.querySelector('.resultado').innerText;
         telaFinal.innerText += this.innerText;            
