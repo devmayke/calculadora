80587ec85583aa3a07d3c60a9dbf7ce16ed9b60d
Mayke Ken <devmayke@gmail.com>
2021-03-20 11:57:15 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,53 +1,53 @@
 const tela = document.querySelector('.resultado');
-const botoes =document.querySelectorAll('button');
+const botoes = document.querySelectorAll('button');
 
 
-for(let i = 0; i <botoes.length; i++){
-   botoes[i].addEventListener("click", (e)=>{  
-       destingueTeclas(e)       
-   })
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].addEventListener("click", (e) => {
+        destingueTeclas(e)
+    })
 }
 
-function destingueTeclas(e){
-   if(!isNaN(e.target.innerText)){           
-       tela.innerText += e.target.innerText;
-       console.log(e.target.innerText)
-   }else{
-    switch (e.target.innerText){               
-        case 'Enter':
-            if(tela.innerText === ''){
+function destingueTeclas(e) {
+    if (!isNaN(e.target.innerText)) {
+        tela.innerText += e.target.innerText;
+        console.log(e.target.innerText)
+    } else {
+        switch (e.target.innerText) {
+            case 'Enter':
+                if (tela.innerText === '') {
+                    tela.innerText = '';
+                } else {
+                    tela.innerText = resultado(tela.innerText)
+                }
+                break;
+            case 'C':
                 tela.innerText = '';
-            }else{
-                tela.innerText= resultado(tela.innerText)
-            }            
-            break;
-        case 'C':
-            tela.innerText = '';                
-            break;
-        case '.':  
-            verifica(',')                          
-        break;
-        case ',':  
-            verifica(',')                         
-            break;       
-        default:
-            verifica(e.target.innerText)                      
-    }       
-   }
-}
-
-function verifica(v){
-    if(tela.innerText.substr(tela.innerText.length-1)=== v){  
-        tela.innerText = tela.innerText;
-    }else{
-        tela.innerText += v; 
-    } 
-    if(tela.innerText.indexOf(',') > -1) {        
-        
+                break;
+            case '.':
+                verifica(',')
+                break;
+            case ',':
+                verifica(',')
+                break;
+            default:
+                verifica(e.target.innerText)
+        }
     }
 }
 
-function resultado(resultado){
+function verifica(v) {
+    if (tela.innerText.substr(tela.innerText.length - 1) === v) {
+        tela.innerText = tela.innerText;
+    } else {
+        tela.innerText += v;
+    }
+    if (tela.innerText.indexOf(',') > -1) {
+
+    }
+}
+
+function resultado(resultado) {
     return eval(resultado)
 }
 
